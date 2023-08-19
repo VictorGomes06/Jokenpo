1c238a1091bc82c7263717c62caa373500530ac6
Victor <victor.gomes9@aluno.senai.br>
2023-08-19 15:04:26 -0300

    Primeiro commit

---
+++ src/jokenpo/Jokenpo.java
@@ -0,0 +1,143 @@
+package jokenpo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jokenpo {
+
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		Random sorteio = new Random();
+		
+		int jogador, maquina, quantidadeRodadas, contador, rodadas;
+		int  vitoriaJogador, vitoriaMaquina;
+		String escolha = "S";
+		
+		System.out.println("-------------------------------------------------");
+		System.out.println("                 J O K E N P O                   ");
+		System.out.println("-------------------------------------------------");
+		
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println("-------------------------------------------------");
+		System.out.println(" 1 - Pedra");
+		System.out.println(" 2 - Papel");
+		System.out.println(" 3 - Tesoura");
+		System.out.println("-------------------------------------------------");
+		System.out.println("");
+		
+		while (escolha.equalsIgnoreCase("s")) {
+			
+			System.out.println("-------------------------------------------------");
+			
+			System.out.print("Quantas partidas: ");
+			quantidadeRodadas = ler.nextInt();
+			vitoriaJogador = 0;
+			vitoriaMaquina = 0;
+			contador = 0;
+			maquina = sorteio.nextInt(3) + 1;
+			System.out.println("-------------------------------------------------");
+			
+		while (contador < quantidadeRodadas) {
+			while (quantidadeRodadas % 2 == 0 || quantidadeRodadas < 3) {
+				System.out.println("Por favor n�o digite n�meros pares ou menores que 3!");
+				System.out.println();
+				System.out.print("Digite a quantidade de rodadas: ");
+				quantidadeRodadas = ler.nextInt();
+			}
+			System.out.println("");
+			System.out.println("-------------------------------------------------");
+			System.out.print("Qual sua escolha: ");
+			jogador = ler.nextInt();
+			System.out.println("-------------------------------------------------");
+			
+			if (jogador == 1) {
+				System.out.println("Sua escolha foi pedra.");
+
+			} else if (jogador == 2) {
+				System.out.println("Sua escolha foi papel.");
+				
+			} else if (jogador == 3) {
+				System.out.println("Sua escolha foi tesoura.");
+				System.out.println("");
+			}
+				System.out.println("");
+		
+				if (maquina == 1) {
+					System.out.println("Computador escolheu pedra.");
+					
+				}else if (maquina == 2) {
+					System.out.println("Computador escolheu papel.");
+					
+				}else if (maquina == 3) {
+					System.out.println("Computador escolheu tesoura.");
+					System.out.println("");
+				}
+					rodadas = quantidadeRodadas;
+					System.out.println("");
+			while (quantidadeRodadas != rodadas) {
+				System.out.println("Escolha um valor de 1 at� 3: ");
+				System.out.println("");
+
+				System.out.println("Escolha uma das op��es: ");
+				System.out.println("1 - Pedra");
+				System.out.println("2 - Papel");
+				System.out.println("3 - Tesoura");
+				System.out.println("");
+
+				System.out.println("------------------------");
+				System.out.print("Qual sua escolha: ");
+				jogador = ler.nextInt();
+				System.out.println("------------------------");
+				System.out.println("");
+			}
+			if ((jogador == 1 && maquina == 3) || (jogador == 2 && maquina == 1) || (jogador == 3 && maquina == 2)) {
+				System.out.println("Voc� ganhou essa rodada!!");
+				System.out.println("");
+			
+			}else if ((jogador == 1 && maquina == 2) || (jogador == 2 && maquina == 3) || (jogador == 3 && maquina == 1)) {
+				System.out.println("A maquina ganhou essa rodada!!");
+				System.out.println("");
+				
+			}else {
+				System.out.println("Empatou essa rodada!!");
+			}
+			
+			
+			if ((jogador == 1 && maquina == 3) || (jogador == 2 && maquina == 1) || (jogador == 3 && maquina == 2)) {
+				vitoriaJogador = vitoriaJogador + 1;
+			}else if ((jogador == 1 && maquina == 2) || (jogador == 2 && maquina == 3) || (jogador == 3 && maquina == 1)) {
+				vitoriaMaquina = vitoriaMaquina + 1;
+			}else {
+				System.out.println("Essa rodada deu empate");
+			}
+			
+			contador++;
+			
+			System.out.println("");
+		}
+		if (vitoriaJogador > vitoriaMaquina) {
+			System.out.println("***** Voc� ganhou meu cria *****");
+		}else {
+			System.out.println("***** O computador ganhou *****");
+		}
+			System.out.println("");
+		
+		System.out.print("Voc� deseja jogar mais? S/N");
+		System.out.print("");
+		escolha= ler.next();
+		
+				
+		}
+		while (!escolha.equalsIgnoreCase("s") && !escolha.equalsIgnoreCase("n")) {
+			System.out.println("Por favor digite S caso queira continuar jogando e N caso n�o queira jogar mais !!!");
+			System.out.println();
+			escolha = ler.next();
+			
+		}
+		ler.close();
+	}
+
+}
